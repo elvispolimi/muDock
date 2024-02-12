4dd498394ed47046b5763c2b45769fa659754510
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-12 17:02:31 +0100

    Rework how we resize and fill the molecule data

--- mudock/include/mudock/molecule/atom_coordinates.hpp
+++ mudock/include/mudock/molecule/atom_coordinates.hpp
@@ -6,19 +6,11 @@
 
 namespace mudock {
 
-  /**
-   * This struct represents all the atoms coordinates. All of them are zero-initialized. We expect that
-   * external functions will fill with the actual content.
-  */
   template<template<typename> class container_type>
-  struct atom_coordinates_type {
-    atom_coordinates_type(const std::size_t n);
-    atom_coordinates_type()                                        = default;
-    atom_coordinates_type(atom_coordinates_type&&)                 = default;
-    atom_coordinates_type(const atom_coordinates_type&)            = default;
-    ~atom_coordinates_type()                                       = default;
-    atom_coordinates_type& operator=(atom_coordinates_type&&)      = default;
-    atom_coordinates_type& operator=(const atom_coordinates_type&) = default;
+  class atom_coordinates_type {
+  public:
+    void resize(const std::size_t n);
+    void fill(const coordinate_type value = 0);
 
     container_type<coordinate_type> x;
     container_type<coordinate_type> y;
@@ -30,8 +22,13 @@ namespace mudock {
   //===------------------------------------------------------------------------------------------------------
 
   template<>
-  atom_coordinates_type<static_container_type>::atom_coordinates_type(const std::size_t n);
+  void atom_coordinates_type<static_container_type>::resize(const std::size_t n);
   template<>
-  atom_coordinates_type<dynamic_container_type>::atom_coordinates_type(const std::size_t n);
+  void atom_coordinates_type<dynamic_container_type>::resize(const std::size_t n);
+
+  template<>
+  void atom_coordinates_type<static_container_type>::fill(const coordinate_type value);
+  template<>
+  void atom_coordinates_type<dynamic_container_type>::fill(const coordinate_type value);
 
 } // namespace mudock
--- mudock/src/molecule/atoms.cpp
+++ mudock/src/molecule/atoms.cpp
@@ -1,14 +1,20 @@
+#include "mudock/type_alias.hpp"
+
 #include <cassert>
 #include <mudock/molecule/atoms.hpp>
 
 namespace mudock {
 
   template<>
-  atoms_type<static_container_type>::atoms_type(const std::size_t n)
-      : coordinates(n), num_atoms(static_cast<index_type>(n)) {}
+  void atoms_type<static_container_type>::resize(const std::size_t n) {
+    coordinates.resize(n);
+    num_atoms = static_cast<index_type>(n);
+  }
 
   template<>
-  atoms_type<dynamic_container_type>::atoms_type(const std::size_t n)
-      : coordinates(n), num_atoms(static_cast<index_type>(n)) {}
+  void atoms_type<dynamic_container_type>::resize(const std::size_t n) {
+    coordinates.resize(n);
+    num_atoms = static_cast<index_type>(n);
+  }
 
 } // namespace mudock
