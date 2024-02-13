20df24e1586dbe7966897d0cf09f5783a1d9d18b
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-13 17:05:16 +0100

    Add the bond description in the molecule strcut

--- mudock/src/molecule.cpp
+++ mudock/src/molecule.cpp
@@ -1,3 +1,22 @@
+#include <mudock/chem/bond_types.hpp>
 #include <mudock/molecule.hpp>
+#include <mudock/molecule/constraints.hpp>
+#include <mudock/type_alias.hpp>
 
-namespace mudock {}
+namespace mudock {
+
+  template<>
+  void molecule_type<static_container_type>::resize(const std::size_t n_atoms, std::size_t n_bonds) {
+    atoms.resize(n_atoms);
+    assert(n_bonds < max_static_bonds());
+    num_bonds = static_cast<index_type>(n_bonds);
+  }
+
+  template<>
+  void molecule_type<dynamic_container_type>::resize(const std::size_t n_atoms, std::size_t n_bonds) {
+    atoms.resize(n_atoms);
+    bonds.resize(n_bonds, {index_type{0}, index_type{0}, bond_type::SINGLE});
+    num_bonds = static_cast<index_type>(n_bonds);
+  }
+
+} // namespace mudock
