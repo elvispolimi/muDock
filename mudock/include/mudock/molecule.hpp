068ac3e0ee5bea7f97966ea4424aaafba67b473a
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-13 12:37:06 +0100

    Apply cosmetic changes

--- mudock/include/mudock/molecule.hpp
+++ mudock/include/mudock/molecule.hpp
@@ -15,12 +15,11 @@ namespace mudock {
   };
 
   using dynamic_molecule = molecule_type<dynamic_container_type>;
-  using static_molecule = molecule_type<static_container_type>;
+  using static_molecule  = molecule_type<static_container_type>;
 
   // this is the concept that defines a molecule, which is any molecule_type for which we have defined a
   // special container and we are agnostic about it.
   template<class T>
-  concept molecule = (std::same_as<T, static_molecule> ||
-                      std::same_as<T, dynamic_molecule>);
+  concept molecule = (std::same_as<T, static_molecule> || std::same_as<T, dynamic_molecule>);
 
 } // namespace mudock
