65c30895a41a69fd3d8f7440b19b56547694e3e3
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-14 09:32:12 +0100

    Add RDkit dependency to the mudock library

--- cmake/prerequisite.cmake
+++ cmake/prerequisite.cmake
@@ -9,3 +9,23 @@ FetchContent_Declare(GSL
     GIT_SHALLOW ON
 )
 FetchContent_MakeAvailable(GSL)
+
+
+#########################################################################
+#### Handle RDKit (it doesn't play nice with boost, we need to improvise)
+#########################################################################
+
+function(add_rdkit_dep target)
+    get_target_property(RDKIT_LIB_FULLPATH RDKit::RDGeneral LOCATION)
+    get_filename_component(RDKIT_LIB_DIRPATH "${RDKIT_LIB_FULLPATH}" DIRECTORY)
+    cmake_path(GET RDKIT_LIB_DIRPATH PARENT_PATH RDKIT_INSTALL_PREFIX)
+    target_include_directories(${target} PUBLIC "${RDKIT_INSTALL_PREFIX}/include/rdkit")
+    target_link_directories(${target} PUBLIC "${RDKIT_INSTALL_PREFIX}/lib")
+    target_link_directories(${target} PUBLIC "${RDKIT_INSTALL_PREFIX}/lib64")
+    target_link_libraries(${target} PUBLIC
+        RDKitFileParsers
+        RDKitGraphMol
+        RDKitRDGeneral
+    )
+    message(STATUS "Found RDKit prefix: ${RDKIT_INSTALL_PREFIX}")
+endfunction(add_rdkit_dep)
