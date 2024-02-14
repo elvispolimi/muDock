0571a6426ff8b5ccf8b5540eedaf4da3c3f98b08
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-14 16:00:23 +0100

    Rework the default value of molecules properties, from empty to N/A

--- mudock/src/molecule/property_table.cpp
+++ mudock/src/molecule/property_table.cpp
@@ -4,13 +4,15 @@
 
 namespace mudock {
 
+  property_map::property_map() { map.fill("N/A"); }
+
   void property_map::assign(const property_type name, std::string value) {
     map[to_index(name)] = std::move(value);
   }
 
   void property_map::initialize(const property_type name, std::string value) {
     auto& element = map[to_index(name)];
-    if (element.empty()) {
+    if (element != "N/A") {
       element = std::move(value);
     }
   }
