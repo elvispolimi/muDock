5c863a472df0d14cb86edafde92016293688b7f4
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-21 13:54:48 +0100

    Add the molecule graph using boost

---
+++ mudock/include/mudock/molecule/graph.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/undirected_graph.hpp>
+#include <mudock/molecule/bond.hpp>
+#include <mudock/molecule/fragments.hpp>
+#include <mudock/type_alias.hpp>
+
+namespace mudock {
+
+  // graph description
+  struct molecule_edge_type {
+    index_type bond_index;
+  };
+  struct molecule_vertex_type {
+    index_type atom_index;
+  };
+  using molecule_graph_type = boost::
+      adjacency_list<boost::setS, boost::vecS, boost::undirectedS, molecule_vertex_type, molecule_edge_type>;
+
+  // utility function that build a graph from the molecules bond
+  template<template<typename> class container_type>
+  molecule_graph_type make_graph(const container_type<bond>& bonds) {
+    using vertex_type = typename molecule_graph_type::vertex_descriptor;
+
+    // describe support data structures that we need to compute the fragment mask
+    molecule_graph_type g;
+    std::unordered_map<index_type, vertex_type> index2vertex;
+    const auto get_vertex = [&index2vertex, &g](const auto index_value) {
+      auto vertex_it = index2vertex.find(index_value);
+      if (vertex_it != std::end(index2vertex)) { // it's a new vertex
+        const auto [new_vertex_it, is_inserted] = index2vertex.emplace(index_value, boost::add_vertex(g));
+        assert(is_inserted);
+        vertex_it = new_vertex_it;
+      }
+      assert(vertex_it != std::end(index2vertex));
+      return vertex_it->second;
+    };
+
+    // populate the graph with the molecule topology
+    for (std::size_t i{0}; i < bonds.size(); ++i) {
+      const auto& bond_description = bonds[i];
+      const auto [edge, is_inserted] =
+          boost::add_edge(get_vertex(bond_description.source), get_vertex(bond_description.source), g);
+      assert(is_inserted);
+      g[edge].bond_index = static_cast<index_type>(i);
+    }
+    return g;
+  }
+
+} // namespace mudock
