03b63ba32d59fbaaec3d318cdf52ae0ce80b7bbc
Davide Gadioli <davide.gadioli@polimi.it>
2024-02-26 11:17:54 +0100

    Fix the dot molecule printer

--- mudock/include/mudock/format/dot.hpp
+++ mudock/include/mudock/format/dot.hpp
@@ -16,18 +16,19 @@ namespace mudock {
     void print(molecule_type&& molecule, std::ostream& out) const {
       out << "graph G {" << std::endl;
       out << "  node [ style=rounded ]" << std::endl;
-      for (index_type i{0}; i < molecule.num_atoms; ++i) {
-        out << "  " << i << " [ label=\"" << i << "." << get_description(molecule.elements[i]).symbol
+      for (index_type i{0}; i < molecule.num_atoms(); ++i) {
+        out << "  " << i << " [ label=\"" << i << "." << get_description(molecule.elements(i)).symbol
             << "\" ]" << std::endl;
       }
-      for (index_type i{0}; i < molecule.num_bonds; ++i) {
-        out << "  " << molecule.bonds[i].source << " -- " << molecule.bonds[i].dest << " [";
-        if (molecule.bonds[i].can_rotate) {
+      for (index_type i{0}; i < molecule.num_bonds(); ++i) {
+        const auto& bond = molecule.bonds(i);
+        out << "  " << bond.source << " -- " << bond.dest << " [";
+        if (bond.can_rotate) {
           out << " style=\"bold\" color=\"red\" ";
         } else {
           out << " style=\"dotted\" ";
         }
-        out << "label=\"" << get_description(molecule.bonds[i].type).name << "\"]" << std::endl;
+        out << "label=\"" << get_description(bond.type).name << "\"]" << std::endl;
       }
       out << "}" << std::endl;
     }
