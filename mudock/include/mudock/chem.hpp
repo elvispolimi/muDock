9eb4e2af7d2c50b7083c6bd3ef66519adfd03856
Davide Gadioli <davide.gadioli@polimi.it>
2024-03-11 14:44:35 +0100

    Fix missing include in chem.hpp

--- mudock/include/mudock/chem.hpp
+++ mudock/include/mudock/chem.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mudock/chem/assign_autodock_babel_types.hpp>
 #include <mudock/chem/autodock_babel_types.hpp>
 #include <mudock/chem/bond_types.hpp>
 #include <mudock/chem/elements.hpp>
