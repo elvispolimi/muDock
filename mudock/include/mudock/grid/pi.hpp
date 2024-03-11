8bea7845b550bf661f0da14f1f65ad39dc50e305
Davide Gadioli <davide.gadioli@polimi.it>
2024-03-11 14:07:49 +0100

    Add a common struct and operations to deal with 3D points

---
+++ mudock/include/mudock/grid/pi.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+#include <mudock/type_alias.hpp>
+
+namespace mudock {
+  struct math {
+    static constexpr auto pi = coordinate_type{3.141592653589793238462643383279502884197};
+  };
+
+  constexpr auto rad_to_deg(const coordinate_type angle) { return angle * math::pi / coordinate_type{180}; }
+} // namespace mudock
