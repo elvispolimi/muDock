4b61a2b1bcb0b5976f5d054935caa9a8590f2425
Davide Gadioli <davide.gadioli@polimi.it>
2024-03-11 15:24:32 +0100

    Fix the rad to def utility function

--- mudock/include/mudock/grid/pi.hpp
+++ mudock/include/mudock/grid/pi.hpp
@@ -8,5 +8,5 @@ namespace mudock {
     static constexpr auto pi = coordinate_type{3.141592653589793238462643383279502884197};
   };
 
-  constexpr auto rad_to_deg(const coordinate_type angle) { return angle * math::pi / coordinate_type{180}; }
+  constexpr auto rad_to_deg(const coordinate_type angle) { return angle * coordinate_type{180} / math::pi; }
 } // namespace mudock
