a9fc49a41e9ab388fcc424dd05238246a095cf47
Davide Gadioli <davide.gadioli@polimi.it>
2024-03-11 14:44:55 +0100

    Fix span parameter types

--- mudock/include/mudock/chem/assign_autodock_babel_types.hpp
+++ mudock/include/mudock/chem/assign_autodock_babel_types.hpp
@@ -8,11 +8,11 @@
 
 namespace mudock {
 
-  void assign_autodock_babel_types(std::span<autodock_babel_ff>& types,
-                                   const std::span<coordinate_type>& x,
-                                   const std::span<coordinate_type>& y,
-                                   const std::span<coordinate_type>& z,
-                                   const std::span<element>& elements,
+  void assign_autodock_babel_types(std::span<autodock_babel_ff> types,
+                                   const std::span<coordinate_type> x,
+                                   const std::span<coordinate_type> y,
+                                   const std::span<coordinate_type> z,
+                                   const std::span<element> elements,
                                    const molecule_graph_type& graph);
 
 }
