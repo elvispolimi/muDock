985911e957b7db9866b4a006ed824268df0f3e46
gianfi12 <10587213@polimi.it>
2024-07-04 18:11:35 +0200

    Add script autodock type

--- script/source_generation/generate_autodock_babel_typing.py
+++ script/source_generation/generate_autodock_babel_typing.py
@@ -4,7 +4,7 @@ import jinja2
 
 # define path to stuff
 script_dirpath = pathlib.Path(__file__).parent
-json_filepath = script_dirpath.parent.parent / "chem" / "autodock_types.json"
+json_filepath = script_dirpath.parent.parent / "chem" / "autodock_babel_types.json"
 template_dirpath = script_dirpath / "templates"
 header_source_filepath = (
     script_dirpath.parent.parent
@@ -12,12 +12,11 @@ header_source_filepath = (
     / "include"
     / "mudock"
     / "chem"
-    / "autodock_types.hpp"
+    / "autodock_babel_types.hpp"
 )
 source_source_filepath = (
-    script_dirpath.parent.parent / "mudock" / "src" / "chem" / "autodock_types.cpp"
+    script_dirpath.parent.parent / "mudock" / "src" / "chem" / "autodock_babel_types.cpp"
 )
-
 
 # initialize the jinja environement
 env = jinja2.Environment(
@@ -36,9 +35,9 @@ for index, element in enumerate(data["ff_type"]):
 
 # render the elements sources
 context = {"data": data["ff_type"], "num_elements": len(data["ff_type"])}
-template = env.get_template("autodock_types.template.hpp")
+template = env.get_template("autodock_babel_types.template.hpp")
 with open(header_source_filepath, "w") as outfile:
     outfile.write(template.render(context))
-    template = env.get_template("autodock_types.template.cpp")
+template = env.get_template("autodock_babel_types.template.cpp")
 with open(source_source_filepath, "w") as outfile:
     outfile.write(template.render(context))
