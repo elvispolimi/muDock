68d39ba376f080950fcbe5d023329d06a6749231
Davide Gadioli <davide.gadioli@polimi.it>
2024-07-16 19:05:54 +0200

    Refactor the code to remove the kernel interface

--- mudock/include/mudock/mudock.hpp
+++ mudock/include/mudock/mudock.hpp
@@ -6,7 +6,6 @@
 #include <mudock/devices.hpp>
 #include <mudock/format.hpp>
 #include <mudock/grid.hpp>
-#include <mudock/kernel.hpp>
 #include <mudock/molecule.hpp>
 #include <mudock/splitter.hpp>
 #include <mudock/type_alias.hpp>
