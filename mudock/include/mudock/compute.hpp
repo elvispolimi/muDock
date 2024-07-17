38b2d9989549a6e0a8b41037455c079e1f9fe78f
Davide Gadioli <davide.gadioli@polimi.it>
2024-07-17 10:42:23 +0200

    Add an header with the device name tokens

--- mudock/include/mudock/compute.hpp
+++ mudock/include/mudock/compute.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mudock/compute/device_names.hpp>
 #include <mudock/compute/reorder_buffer.hpp>
 #include <mudock/compute/safe_stack.hpp>
 #include <mudock/compute/threadpool.hpp>
