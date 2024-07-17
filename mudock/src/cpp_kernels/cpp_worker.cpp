0ffad89b1a43f44aa8c4cda1393fb93cc696345c
Davide Gadioli <davide.gadioli@polimi.it>
2024-07-17 09:44:41 +0200

    Promote ref to queues to shared_ptr for improving robustness

--- mudock/src/cpp_kernels/cpp_worker.cpp
+++ mudock/src/cpp_kernels/cpp_worker.cpp
@@ -5,9 +5,9 @@
 #include <string>
 
 namespace mudock {
-  cpp_worker::cpp_worker(std::shared_ptr<dynamic_molecule>& protein,
-                         safe_stack<static_molecule>& input_molecules,
-                         safe_stack<static_molecule>& output_molecules,
+  cpp_worker::cpp_worker(std::shared_ptr<dynamic_molecule> protein,
+                         std::shared_ptr<safe_stack<static_molecule>> input_molecules,
+                         std::shared_ptr<safe_stack<static_molecule>> output_molecules,
                          const std::size_t cpu_id)
       : input_stack(input_molecules), output_stack(output_molecules), virtual_screen(protein) {
     cpu_set_t cpuset;
@@ -17,16 +17,16 @@ namespace mudock {
   }
 
   void cpp_worker::main() {
-    auto new_ligand = input_stack.dequeue();
+    auto new_ligand = input_stack->dequeue();
     while (new_ligand) {
       virtual_screen(*new_ligand);
       try {
-        output_stack.enqueue(std::move(new_ligand));
+        output_stack->enqueue(std::move(new_ligand));
       } catch (const std::runtime_error& e) {
         std::cerr << std::string{"Unable to vs molecule "} + new_ligand->properties.get(property_type::NAME) +
                          std::string{" due to: "} + e.what() + std::string{"\n"};
       }
-      new_ligand = std::move(input_stack.dequeue());
+      new_ligand = std::move(input_stack->dequeue());
     }
   }
 
