47650726d9b8b1071e9dad83580bdc4f22fb2728
Davide Gadioli <davide.gadioli@polimi.it>
2024-07-17 17:52:54 +0200

    Fix typo in the compute_batch_size function definition

--- mudock/src/cuda_implementation/cuda_batch_sizer.cu
+++ mudock/src/cuda_implementation/cuda_batch_sizer.cu
@@ -1,7 +1,8 @@
 #include <mudock/cuda_implementation/cuda_batch_sizer.cuh>
 
 namespace mudock {
-  std::size_t copute_batch_size(const std::size_t num_atoms, const std::size_t num_rotamers) {
+  std::size_t compute_batch_size([[maybe_unused]] const std::size_t num_atoms,
+                                 [[maybe_unused]] const std::size_t num_rotamers) {
     // TODO make something useful
     return 10;
   }
