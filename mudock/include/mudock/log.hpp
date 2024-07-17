42af2d3edbbd233d45ce5bc1fb0bbc53e0578a6b
Davide Gadioli <davide.gadioli@polimi.it>
2024-07-17 13:08:48 +0200

    Fix the log time output

--- mudock/include/mudock/log.hpp
+++ mudock/include/mudock/log.hpp
@@ -15,16 +15,23 @@ namespace mudock {
       log_add_line(s, remainder...);
     }
 
+    class timer {
+      static std::chrono::steady_clock::time_point start;
+
+    public:
+      static inline float get() {
+        return std::chrono::duration<float>(std::chrono::steady_clock::now() - start).count();
+      }
+    };
+
     template<class... Ts>
     void log(Ts&&... args) {
       // declare the string stream and line composer for our log function
       std::ostringstream stream;
 
-      // start by printing the time since the first time we called the log function
-      static const auto start = std::chrono::steady_clock::now();
-      const auto stop         = std::chrono::steady_clock::now();
+      // start by printing the elapsed time
       stream << '[' << std::fixed << std::setprecision(2) << std::setw(12) << std::setfill(' ')
-             << std::chrono::duration<float>(stop - start).count() << "] ";
+             << timer::get() << "] ";
       log_add_line(stream, args...);
 
       // print in output the line
