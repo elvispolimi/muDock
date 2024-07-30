ca3ccd4e48a60c7f8714a12c4aca4c84ec27a521
gianfi12 <10587213@polimi.it>
2024-07-30 10:08:48 +0200

    Fix template

--- script/source_generation/templates/autodock_parameters.template.hpp
+++ script/source_generation/templates/autodock_parameters.template.hpp
@@ -4,10 +4,10 @@
 
 namespace mudock {
   struct autodock_parameters {
-    static constexpr fp_type FE_coeff_vdW{{@ FE_coeff_vdW @}};
-    static constexpr fp_type FE_coeff_hbond{{@ FE_coeff_hbond @}};
-    static constexpr fp_type FE_coeff_estat{{@ FE_coeff_estat @}};
-    static constexpr fp_type FE_coeff_desolv{{@ FE_coeff_desolv @}};
-    static constexpr fp_type FE_coeff_tors{{@ FE_coeff_tors @}};
+    static constexpr fp_type coeff_vdW{{@ FE_coeff_vdW @}};
+    static constexpr fp_type coeff_hbond{{@ FE_coeff_hbond @}};
+    static constexpr fp_type coeff_estat{{@ FE_coeff_estat @}};
+    static constexpr fp_type coeff_desolv{{@ FE_coeff_desolv @}};
+    static constexpr fp_type coeff_tors{{@ FE_coeff_tors @}};
   };
 } // namespace mudock
