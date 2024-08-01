a8a2bb0e1700b11114bc35232fff284a12712169
gianfi12 <10587213@polimi.it>
2024-08-01 17:49:58 +0200

    Update autodock parameters following autodock default_parameters.h

--- mudock/include/mudock/chem/autodock_parameters.hpp
+++ mudock/include/mudock/chem/autodock_parameters.hpp
@@ -4,10 +4,11 @@
 
 namespace mudock {
   struct autodock_parameters {
-    static constexpr fp_type coeff_vdW{0.1560};
-    static constexpr fp_type coeff_hbond{0.0974};
-    static constexpr fp_type coeff_estat{0.1465};
-    static constexpr fp_type coeff_desolv{0.1159};
-    static constexpr fp_type coeff_tors{0.2744};
+    // TODO hardcode from param_string_4_1
+    static constexpr fp_type coeff_vdW{0.1662};
+    static constexpr fp_type coeff_hbond{0.1209};
+    static constexpr fp_type coeff_estat{0.1406};
+    static constexpr fp_type coeff_desolv{0.1322};
+    static constexpr fp_type coeff_tors{0.2983};
   };
 } // namespace mudock
