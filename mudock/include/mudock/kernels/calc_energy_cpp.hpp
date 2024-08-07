ddee1359c66bee9e120a424e4ee81d4e92bd0e87
gianfi12 <10587213@polimi.it>
2024-08-07 12:45:30 +0200

    Add autodock type field on molecule, and update grid map creatio

--- mudock/include/mudock/kernels/calc_energy_cpp.hpp
+++ mudock/include/mudock/kernels/calc_energy_cpp.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "mudock/grid/grid_map.hpp"
+
 #include <mudock/grid.hpp>
 #include <mudock/molecule.hpp>
 #include <vector>
 
 namespace mudock {
 
-  void calc_energy(const static_molecule& receptor, const dynamic_molecule& ligand, const std::vector<grid_map> grid_maps);
+  void calc_energy(const static_molecule& receptor,
+                   const dynamic_molecule& ligand,
+                   const grid_atom_mapper& grid_atom_maps,
+                   const grid_map& electro_map,
+                   const grid_map& desolv_map);
 
 }
