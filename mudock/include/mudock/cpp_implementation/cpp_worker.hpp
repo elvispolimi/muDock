76f73eabc1f027bf6d6cfa9a21eb76f667d13a88
gianfi12 <10587213@polimi.it>
2024-08-15 16:25:41 +0200

    Fix previous commit with also header updated

--- mudock/include/mudock/cpp_implementation/cpp_worker.hpp
+++ mudock/include/mudock/cpp_implementation/cpp_worker.hpp
@@ -17,7 +17,10 @@ namespace mudock {
 
   public:
     // the constructor intialize the kernel and set the CPU affinity to the correct device
-    cpp_worker(std::shared_ptr<dynamic_molecule> protein,
+    cpp_worker(std::shared_ptr<const dynamic_molecule> protein,
+               std::shared_ptr<const grid_atom_mapper> grid_atom_maps,
+               std::shared_ptr<const grid_map> electro_map,
+               std::shared_ptr<const grid_map> desolv_map,
                std::shared_ptr<safe_stack<static_molecule>> input_molecules,
                std::shared_ptr<safe_stack<static_molecule>> output_molecules,
                const std::size_t cpu_id);
--- mudock/include/mudock/cpp_implementation/virtual_screen.hpp
+++ mudock/include/mudock/cpp_implementation/virtual_screen.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <mudock/grid.hpp>
 #include <mudock/molecule.hpp>
 #include <mudock/type_alias.hpp>
 #include <random>
@@ -15,8 +16,16 @@ namespace mudock {
     std::mt19937 generator;
     std::uniform_real_distribution<fp_type> dist;
 
+    std::shared_ptr<const dynamic_molecule>& protein;
+    std::shared_ptr<const grid_atom_mapper> grid_atom_maps;
+    std::shared_ptr<const grid_map> electro_map;
+    std::shared_ptr<const grid_map> desolv_map;
+
   public:
-    virtual_screen_cpp(std::shared_ptr<dynamic_molecule>& protein);
+    virtual_screen_cpp(std::shared_ptr<const dynamic_molecule>& protein,
+                       std::shared_ptr<const grid_atom_mapper> grid_atom_maps,
+                       std::shared_ptr<const grid_map> electro_map,
+                       std::shared_ptr<const grid_map> desolv_map);
 
     void operator()(static_molecule& ligand);
   };
