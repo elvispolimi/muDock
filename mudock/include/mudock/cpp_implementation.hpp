b162cc962f8dcfd538fd5e69816d64b9bb17db29
Davide Gadioli <davide.gadioli@polimi.it>
2024-08-27 17:57:58 +0200

    Add the chromosome class

--- mudock/include/mudock/cpp_implementation.hpp
+++ mudock/include/mudock/cpp_implementation.hpp
@@ -1,3 +1,4 @@
+#include <mudock/cpp_implementation/chromosome.hpp>
 #include <mudock/cpp_implementation/cpp_manager.hpp>
 #include <mudock/cpp_implementation/cpp_worker.hpp>
 #include <mudock/cpp_implementation/geometric_transformations.hpp>
---
+++ mudock/include/mudock/cpp_implementation/chromosome.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <mudock/molecule.hpp>
+#include <mudock/type_alias.hpp>
+
+namespace mudock {
+
+  /**
+   * A Chromosome represents a geometric transformation that we apply to alter
+   * the 3D displacement and shape of a given molecule. In this version we use
+   * the following genes:
+   *  0 -> Offset (in Angstrom) of a translation along the x axys
+   *  1 -> Offset (in Angstrom) of a translation along the y axys
+   *  2 -> Offset (in Angstrom) of a translation along the z axys
+   *  3 -> Angle (in degree) that we rotate the whole molecule along the x axys
+   *  4 -> Angle (in degree) that we rotate the whole molecule along the y axys
+   *  5 -> Angle (in degree) that we rotate the whole molecule along the z axys
+   *  6 -> Angle (in degree) that we rotate a fragment along the first rotable bond (if any)
+   *  7 -> Angle (in degree) that we rotate a fragment along the second rotable bond (if any)
+   *  8 -> Angle (in degree) that we rotate a fragment along the third rotatable bond (if any)
+   *  9 -> ...
+   *
+   * The actual number of genes depends on the given molecule
+  */
+  class chromosome {
+    std::array<fp_type, 6 + max_static_bonds()> genes;
+    std::size_t size = 6;
+  };
+
+} // namespace mudock
