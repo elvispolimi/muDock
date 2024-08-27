5f78195452c0d93ebf6588ad69798366b2e5342d
Davide Gadioli <davide.gadioli@polimi.it>
2024-08-27 17:44:04 +0200

    Add geometric transformation for the molecule and fragments

--- mudock/include/mudock/cpp_implementation.hpp
+++ mudock/include/mudock/cpp_implementation.hpp
@@ -1,3 +1,4 @@
 #include <mudock/cpp_implementation/cpp_manager.hpp>
 #include <mudock/cpp_implementation/cpp_worker.hpp>
+#include <mudock/cpp_implementation/geometric_transformations.hpp>
 #include <mudock/cpp_implementation/virtual_screen.hpp>
