a3fb8444c2ab1d1ae36b2b3beb7d05982c87fc23
Davide Gadioli <davide.gadioli@polimi.it>
2024-08-28 15:27:39 +0200

    Add a function to compute the center of mass of a 3D point list

--- mudock/include/mudock/cpp_implementation.hpp
+++ mudock/include/mudock/cpp_implementation.hpp
@@ -1,3 +1,4 @@
+#include <mudock/cpp_implementation/center_of_mass.hpp>
 #include <mudock/cpp_implementation/chromosome.hpp>
 #include <mudock/cpp_implementation/cpp_manager.hpp>
 #include <mudock/cpp_implementation/cpp_worker.hpp>
