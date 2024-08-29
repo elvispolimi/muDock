5df4ad06ce0c3e95ff583a0f13730ad9f338883b
gianfi12 <10587213@polimi.it>
2024-08-29 13:27:08 +0200

    Use energy value for genetic algorithm

--- mudock/src/cpp_implementation/virtual_screen.cpp
+++ mudock/src/cpp_implementation/virtual_screen.cpp
@@ -115,7 +115,7 @@ namespace mudock {
                                         *grid_atom_maps,
                                         *electro_map,
                                         *desolv_map);
-        // element.score = altered_x[0]; // dummy implementation to test the genetic
+        element.score = energy; // dummy implementation to test the genetic
       }
 
       // Generate the new population
