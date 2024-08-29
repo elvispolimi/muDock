a6541c8d33b02ecf11ad105c83f3eb61d2956b17
Davide Gadioli <davide.gadioli@polimi.it>
2024-08-29 16:26:56 +0200

    Rename data in _data in mdvector

--- mudock/include/mudock/grid/mdvector.hpp
+++ mudock/include/mudock/grid/mdvector.hpp
@@ -11,22 +11,22 @@ namespace mudock {
    */
   template<class T, std::size_t n>
   class md_vector: public md_index<n> {
-    std::vector<T> data;
+    std::vector<T> _data;
 
   public:
     // data constructor
     md_vector(): md_index<n>() {}
     template<class... Y>
-    md_vector(Y&&... sizes): md_index<n>(sizes...), data(md_index<n>::flat_size()) {}
+    md_vector(Y&&... sizes): md_index<n>(sizes...), _data(md_index<n>::flat_size()) {}
 
     // operator to retrieve data
     template<class... Y>
     [[nodiscard]] auto& get(Y&&... indexes) {
-      return data[md_index<n>::to1D(indexes...)];
+      return _data[md_index<n>::to1D(indexes...)];
     }
     template<class... Y>
     [[nodiscard]] const auto& get(Y&&... indexes) const {
-      return data[md_index<n>::to1D(indexes...)];
+      return _data[md_index<n>::to1D(indexes...)];
     }
   };
 
