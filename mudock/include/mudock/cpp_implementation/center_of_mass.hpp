0cb31b6dd202d70e369ab4e2d3516b4af845b8a5
Davide Gadioli <davide.gadioli@polimi.it>
2024-09-05 14:10:23 +0200

    Fix the signature of compute_center_of_mass

--- mudock/include/mudock/cpp_implementation/center_of_mass.hpp
+++ mudock/include/mudock/cpp_implementation/center_of_mass.hpp
@@ -7,9 +7,9 @@
 
 namespace mudock {
 
-  inline point3D compute_center_of_mass(const std::span<fp_type> x,
-                                        const std::span<fp_type> y,
-                                        const std::span<fp_type> z) {
+  inline point3D compute_center_of_mass(const std::span<const fp_type> x,
+                                        const std::span<const fp_type> y,
+                                        const std::span<const fp_type> z) {
     return point3D{std::accumulate(std::begin(x), std::end(x), fp_type{0}) / static_cast<fp_type>(x.size()),
                    std::accumulate(std::begin(y), std::end(y), fp_type{0}) / static_cast<fp_type>(y.size()),
                    std::accumulate(std::begin(z), std::end(z), fp_type{0}) / static_cast<fp_type>(z.size())};
