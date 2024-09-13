35da7f67679f8cb73aede86567233962d5e5e884
gianfi12 <10587213@polimi.it>
2024-09-13 13:21:57 +0200

    Rework to use int type

--- mudock/include/mudock/grid/mdindex.hpp
+++ mudock/include/mudock/grid/mdindex.hpp
@@ -12,39 +12,37 @@ namespace mudock {
   //===------------------------------------------------------------------------------------------------------
 
   struct index {
-    virtual std::size_t
-        to1D(const std::size_t index_x, const std::size_t index_y, const std::size_t index_z) const = 0;
-    virtual std::size_t to1D(const std::size_t index_x, const std::size_t index_y) const            = 0;
-    virtual std::size_t get_dim() const                                                             = 0;
+    virtual int to1D(const int index_x, const int index_y, const int index_z) const = 0;
+    virtual int to1D(const int index_x, const int index_y) const                    = 0;
+    virtual int get_dim() const                                                     = 0;
 
     virtual ~index() = default;
   };
 
   class index2D: public index {
-    std::size_t n_x = 0;
-    std::size_t n_y = 0;
+    int n_x = 0;
+    int n_y = 0;
 
   public:
-    inline index2D(const std::size_t size_x = 0, const std::size_t size_y = 0): n_x(size_x), n_y(size_y) {}
-    [[nodiscard]] inline std::size_t to1D(const std::size_t index_x,
-                                          const std::size_t index_y) const override {
+    inline index2D(const int size_x = 0, const int size_y = 0): n_x(size_x), n_y(size_y) {}
+    [[nodiscard]] inline int to1D(const int index_x, const int index_y) const override {
       assert(index_x < n_x);
       assert(index_y < n_y);
       return index_y * n_x + index_x;
     }
-    [[nodiscard]] inline std::size_t to1D([[maybe_unused]] const std::size_t index_x,
-                                          [[maybe_unused]] const std::size_t index_y,
-                                          [[maybe_unused]] const std::size_t index_z) const override {
+    [[nodiscard]] inline int to1D([[maybe_unused]] const int index_x,
+                                  [[maybe_unused]] const int index_y,
+                                  [[maybe_unused]] const int index_z) const override {
       throw std::logic_error("This method is not applicable for 2D indexing.");
     }
-    [[nodiscard]] inline auto to2D(const std::size_t index1D) const {
+    [[nodiscard]] inline auto to2D(const int index1D) const {
       assert(index1D < n_x * n_y);
       return std::make_tuple(index1D % n_x, index1D / n_x);
     }
     [[nodiscard]] inline auto size_x() const { return n_x; }
     [[nodiscard]] inline auto size_y() const { return n_y; }
 
-    [[nodiscard]] inline std::size_t get_dim() const override { return n_x * n_y; }
+    [[nodiscard]] inline int get_dim() const override { return n_x * n_y; }
   };
 
   //===------------------------------------------------------------------------------------------------------
@@ -52,26 +50,28 @@ namespace mudock {
   //===------------------------------------------------------------------------------------------------------
 
   class index3D: public index {
-    std::size_t n_x  = 0;
-    std::size_t n_y  = 0;
-    std::size_t n_z  = 0;
-    std::size_t n_xy = 0;
+    int n_x  = 0;
+    int n_y  = 0;
+    int n_z  = 0;
+    int n_xy = 0;
 
   public:
-    inline index3D(const std::size_t size_x = 0, const std::size_t size_y = 0, const std::size_t size_z = 0)
+    inline index3D(const int size_x = 0, const int size_y = 0, const int size_z = 0)
         : n_x(size_x), n_y(size_y), n_z(size_z), n_xy(size_x * size_y) {}
-    [[nodiscard]] inline std::size_t to1D([[maybe_unused]] const std::size_t index_x,
-                                          [[maybe_unused]] const std::size_t index_y) const override {
+    [[nodiscard]] inline int to1D([[maybe_unused]] const int index_x,
+                                  [[maybe_unused]] const int index_y) const override {
       throw std::logic_error("This method is not applicable for 3D indexing.");
     }
-    [[nodiscard]] inline std::size_t
-        to1D(const std::size_t index_x, const std::size_t index_y, const std::size_t index_z) const override {
+
+    bool operator==(const index3D& d) const { return n_x == d.n_x && n_y == d.n_y && n_z == d.n_z; }
+
+    [[nodiscard]] inline int to1D(const int index_x, const int index_y, const int index_z) const override {
       assert(index_x < n_x);
       assert(index_y < n_y);
       assert(index_z < n_z);
       return n_xy * index_z + index_y * n_x + index_x;
     }
-    [[nodiscard]] inline auto to3D(const std::size_t index1D) const {
+    [[nodiscard]] inline auto to3D(const int index1D) const {
       assert(index1D < n_x * n_y * n_z);
       return std::make_tuple(index1D % n_x, (index1D % n_xy) / n_y, index1D / n_xy);
     }
@@ -79,7 +79,7 @@ namespace mudock {
     [[nodiscard]] inline auto size_y() const { return n_y; }
     [[nodiscard]] inline auto size_z() const { return n_z; }
 
-    [[nodiscard]] inline std::size_t get_dim() const override { return n_xy * n_z; }
+    [[nodiscard]] inline int get_dim() const override { return n_xy * n_z; }
   };
 
   template<class T>
