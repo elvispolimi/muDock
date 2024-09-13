51dca7b96db40cc117182de152710b1251ad97b0
gianfi12 <10587213@polimi.it>
2024-09-13 12:11:46 +0200

    Update grid const for kernel calls

--- mudock/include/mudock/chem/grid_const.hpp
+++ mudock/include/mudock/chem/grid_const.hpp
@@ -14,16 +14,16 @@ namespace mudock {
   static constexpr fp_type unknown_distance_3 = 2.89; //Angstrom
   static constexpr fp_type unknown_distance_4 = 1.69; //Angstrom
 
-  static constexpr std::size_t range_near_atom_receptor = 20;
+  static constexpr int range_near_atom_receptor = 20;
 
   // TODO why this value?
-  static constexpr size_t NEINT{
+  static constexpr int NEINT{
       2048}; /* Number of values in internal energy table, they are based on radius range values */
   /* Number of dielectric and desolvation values in lookup table.
   NDIEL is bigger than NEINT because electrostatic interactions are much
   longer-range than van der Waals interactions. */
   // TODO wrong comment in autogrid
-  static constexpr size_t NDIEL{16384};
+  static constexpr int NDIEL{16384};
 
   static constexpr fp_type NBC{8.00}; /* Non-bonded cutoff for internal energy calc./Ang*/
 
@@ -37,4 +37,13 @@ namespace mudock {
   static constexpr fp_type sigma{3.6};
 
   static constexpr fp_type precision{0.0001};
+
+  static constexpr fp_type RMIN_ELEC{0.5};
+  static constexpr fp_type ELECSCALE{332.06363};
+  static constexpr fp_type qsolpar{0.01097};
+  static constexpr fp_type r_smooth{
+      0.0}; // vdw nonbond smoothing range, not radius, Ang - default 0.5 matches AutoGrid recommendations
+  // Non bond cutoff
+  static constexpr fp_type nbc2{64}; // 8*8
+  static constexpr fp_type ENERGYPENALTY{500};
 } // namespace mudock
