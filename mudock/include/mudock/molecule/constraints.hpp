af0c31a4fd9a7c4f27d9f30a8ae1b0272b6e6f2f
gianfi12 <10587213@polimi.it>
2024-09-13 13:22:20 +0200

    Rework to use int type

--- mudock/include/mudock/molecule/constraints.hpp
+++ mudock/include/mudock/molecule/constraints.hpp
@@ -5,9 +5,9 @@
 namespace mudock {
 
   // this is the maximum number of atoms that we can expect from a static storage
-  [[nodiscard]] constexpr auto max_static_atoms() { return std::size_t{255}; }
+  [[nodiscard]] constexpr auto max_static_atoms() { return int{255}; }
 
   // this is the maximum number of bonds that we can expect from a static storage
-  [[nodiscard]] constexpr auto max_static_bonds() { return max_static_atoms() * std::size_t{2}; }
+  [[nodiscard]] constexpr auto max_static_bonds() { return max_static_atoms() * int{2}; }
 
 } // namespace mudock
