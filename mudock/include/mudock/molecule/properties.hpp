fa94283a5b1cac0544807727c01814bb15bfead0
gianfi12 <10587213@polimi.it>
2024-09-13 13:22:20 +0200

    Rework to use int type

--- mudock/include/mudock/molecule/properties.hpp
+++ mudock/include/mudock/molecule/properties.hpp
@@ -4,9 +4,7 @@
 #include <cstdint>
 
 namespace mudock {
-  enum class property_type : std::size_t { NAME = 0, SCORE, END };
+  enum class property_type : int { NAME = 0, SCORE, END };
 
-  [[nodiscard]] constexpr std::size_t property_type_size() {
-    return static_cast<std::size_t>(property_type::END);
-  }
+  [[nodiscard]] constexpr int property_type_size() { return static_cast<int>(property_type::END); }
 } // namespace mudock
