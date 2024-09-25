29885d0fb48c4f8745517f2aed30fecab8294cf9
gianfi12 <10587213@polimi.it>
2024-09-25 13:58:06 +0900

    Add more CUDA workers

--- mudock/src/cuda_implementation/cuda_manager.cpp
+++ mudock/src/cuda_implementation/cuda_manager.cpp
@@ -74,6 +74,38 @@ namespace mudock {
                                              output_molecules,
                                              rob,
                                              id);
+        pool.add_worker<mudock::cuda_worker>(knobs,
+                                             grid_atom_maps,
+                                             electro_map,
+                                             desolv_map,
+                                             input_molecules,
+                                             output_molecules,
+                                             rob,
+                                             id);
+        pool.add_worker<mudock::cuda_worker>(knobs,
+                                             grid_atom_maps,
+                                             electro_map,
+                                             desolv_map,
+                                             input_molecules,
+                                             output_molecules,
+                                             rob,
+                                             id);
+        pool.add_worker<mudock::cuda_worker>(knobs,
+                                             grid_atom_maps,
+                                             electro_map,
+                                             desolv_map,
+                                             input_molecules,
+                                             output_molecules,
+                                             rob,
+                                             id);
+        pool.add_worker<mudock::cuda_worker>(knobs,
+                                             grid_atom_maps,
+                                             electro_map,
+                                             desolv_map,
+                                             input_molecules,
+                                             output_molecules,
+                                             rob,
+                                             id);
       }
     }
   }
