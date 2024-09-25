6b009519791a19251f2c45d35d00453e076eee18
gianfi12 <10587213@polimi.it>
2024-09-25 11:28:00 +0900

    Change calc energy interface

--- mudock/include/mudock/cuda_implementation/calc_energy.cuh
+++ mudock/include/mudock/cuda_implementation/calc_energy.cuh
@@ -4,7 +4,7 @@
 #include <mudock/type_alias.hpp>
 
 namespace mudock {
-  __device__ void calc_intra_energy(const fp_type* ligand_x,
+  __device__ fp_type calc_intra_energy(const fp_type* ligand_x,
                                     const fp_type* ligand_y,
                                     const fp_type* ligand_z,
                                     const fp_type* ligand_vol,
@@ -18,8 +18,5 @@ namespace mudock {
                                     const int num_atoms,
                                     const int ligand_num_nonbonds,
                                     const int* __restrict__ ligand_nonbond_a1,
-                                    const int* __restrict__ ligand_nonbond_a2,
-                                    fp_type* elect_total_eintcal,
-                                    fp_type* emap_total_eintcal,
-                                    fp_type* dmap_total_eintcal);
+                                    const int* __restrict__ ligand_nonbond_a2);
 }
