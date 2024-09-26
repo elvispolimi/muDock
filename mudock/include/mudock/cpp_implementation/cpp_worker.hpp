256adebb9bcca0e690f7b34675e120e52ad28598
gianfi12 <10587213@polimi.it>
2024-09-26 10:12:56 +0900

    Change cpp worker interface and fix warning clang about copy elision

--- mudock/include/mudock/cpp_implementation/cpp_worker.hpp
+++ mudock/include/mudock/cpp_implementation/cpp_worker.hpp
@@ -18,10 +18,10 @@ namespace mudock {
 
   public:
     // the constructor intialize the kernel and set the CPU affinity to the correct device
-    cpp_worker(std::shared_ptr<const grid_atom_mapper>& grid_atom_maps,
+    cpp_worker(const knobs knobs,
+               std::shared_ptr<const grid_atom_mapper>& grid_atom_maps,
                std::shared_ptr<const grid_map>& electro_map,
                std::shared_ptr<const grid_map>& desolv_map,
-               const knobs knobs,
                std::shared_ptr<safe_stack<static_molecule>>& input_molecules,
                std::shared_ptr<safe_stack<static_molecule>>& output_molecules,
                const std::size_t cpu_id);
