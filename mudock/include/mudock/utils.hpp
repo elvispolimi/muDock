3da52398e049432051cc4430bd8202dc584b752e
gianfi12 <10587213@polimi.it>
2024-09-30 13:53:09 +0900

    Update manage debug mode

--- mudock/include/mudock/utils.hpp
+++ mudock/include/mudock/utils.hpp
@@ -12,7 +12,7 @@ constexpr void constexpr_for(F&& f) {
 
 constexpr auto is_debug() {
   // TODO this should work only with CMAKE
-#ifndef NDEBUG
+#ifdef DEBUG_MODE
   return true;
 #else
   return false;
