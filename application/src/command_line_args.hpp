d765fe84cf60da54af5876ce633ee3933c4c4cfc
gianfi12 <10587213@polimi.it>
2024-10-18 13:41:10 +0900

    Add output flag

--- application/src/command_line_args.hpp
+++ application/src/command_line_args.hpp
@@ -6,6 +6,7 @@
 struct command_line_arguments {
   std::filesystem::path protein_path = std::filesystem::path{"protein.pdb"};
   std::string device_conf            = std::string{"CPP:CPU:0"};
+  std::string result_path;
   mudock::knobs knobs;
 };
 command_line_arguments parse_command_line_arguments(const int argc, char *argv[]);
