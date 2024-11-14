994741f43b945cd488aff06302759c73fa6586c7
gianfi12 <10587213@polimi.it>
2024-11-14 11:23:15 +0900

    Update

--- mudock/include/mudock/chem/mehler_solmajer.hpp
+++ mudock/include/mudock/chem/mehler_solmajer.hpp
@@ -6,18 +6,20 @@
 #include <array>
 
 namespace mudock {
-  static constexpr fp_type ms_lambda{0.003627};
-  static constexpr fp_type ms_epsilon0{78.4};
-  static constexpr fp_type ms_A{-8.5525};
-  static constexpr fp_type ms_B = ms_epsilon0 - ms_A;
-  static constexpr fp_type ms_rk{7.7839};
-  static constexpr fp_type ms_lambda_B = -ms_lambda * ms_B;
-  static constexpr fp_type ms_min_epsilon{std::numeric_limits<fp_type>::epsilon()};
+  namespace mehler_solmajer{
+  static constexpr fp_type lambda{0.003627};
+  static constexpr fp_type epsilon0{78.4};
+  static constexpr fp_type A{-8.5525};
+  static constexpr fp_type B = epsilon0 - A;
+  static constexpr fp_type rk{7.7839};
+  static constexpr fp_type lambda_B = -lambda * B;
+  static constexpr fp_type min_epsilon{std::numeric_limits<fp_type>::epsilon()};
+  }
 
   inline fp_type calc_ddd_Mehler_Solmajer(const fp_type& distance) {
-    fp_type epsilon = ms_A + ms_B / (fp_type{1} + ms_rk * std::exp(ms_lambda_B * distance));
+    fp_type epsilon = mehler_solmajer::A + mehler_solmajer::B / (fp_type{1} + mehler_solmajer::rk * std::exp(mehler_solmajer::lambda_B * distance));
 
-    if (epsilon < ms_min_epsilon) [[unlikely]] {
+    if (epsilon < mehler_solmajer::min_epsilon) [[unlikely]] {
       epsilon = fp_type{1.0};
     }
     return epsilon;
