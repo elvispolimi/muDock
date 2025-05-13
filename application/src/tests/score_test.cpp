cbc5faf42254065e5219f770f48587130e85643b
gaccordi <10587213@polimi.it>
2025-05-13 14:54:23 +0200

    Polish test code

--- application/src/tests/score_test.cpp
+++ application/src/tests/score_test.cpp
@@ -58,15 +58,6 @@ int main(int argc, char* argv[]) {
   auto electrostatic_map = std::make_shared<const mudock::grid_map>(generate_electrostatic_grid_map(protein));
   auto desolvation_map   = std::make_shared<const mudock::grid_map>(generate_desolvation_grid_map(protein));
 
-  // auto tp  = mudock::point3D{};
-  // auto itp = mudock::index3D{1, 1, 1};
-  // auto sv  = std::vector<mudock::grid_atom_map>{};
-  // for (int i = 0; i < mudock::num_ligand_map_types(); ++i)
-  //   sv.emplace_back(mudock::autodock_type_from_map(static_cast<mudock::ligand_map_types>(i)), protein);
-  // auto grid_atom_maps    = std::make_shared<const mudock::grid_atom_mapper>(sv);
-  // auto electrostatic_map = std::make_shared<const mudock::grid_map>(protein);
-  // auto desolvation_map   = std::make_shared<const mudock::grid_map>(protein);
-
   mudock::info("Reading and parsing ligand ", ligand_path, " ...");
   auto ligand = std::make_unique<mudock::static_molecule>();
   mudock::parse(*ligand, ligand_path);
