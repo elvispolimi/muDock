ee6d2fe54495c7a3044db2805abbecbb802eb8f9
gianfi12 <10587213@polimi.it>
2025-10-01 00:46:00 +0900

    Fix virtual screening fragments indices stack corruption

--- mudock/src/hip_implementation/virtual_screen.cpp
+++ mudock/src/hip_implementation/virtual_screen.cpp
@@ -69,11 +69,11 @@ namespace mudock {
     scratch_ligand_y.alloc(tot_atoms_in_batch);
     scratch_ligand_z.alloc(tot_atoms_in_batch);
     ligand_fragments.alloc(tot_rotamers_atoms_in_batch);
-    ligand_fragments_start.alloc(batch_ligands);
+    ligand_fragments_start.alloc(batch_ligands + 1);
     ligand_fragments_start()[0] = 0;
     frag_start_atom_indices.alloc(tot_rotamers_in_batch);
     frag_stop_atom_indices.alloc(tot_rotamers_in_batch);
-    frag_indices_start.alloc(batch_ligands);
+    frag_indices_start.alloc(batch_ligands + 1);
     frag_indices_start()[0] = 0;
     ligand_vol.alloc(tot_atoms_in_batch);
     ligand_solpar.alloc(tot_atoms_in_batch);
