88a28b98d4207935cb82074fe1ba9f0e0589b91a
gaccordi <10587213@polimi.it>
2025-12-13 16:18:55 -0800

    Adapt cpp implementation to pipeline version

---
+++ mudock/include/mudock/cpp_implementation/adt_score_cpp.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <mudock/compute/adt_score.hpp>
+#include <mudock/cpp_implementation/queue_cpp.hpp>
+
+namespace mudock {
+
+  template<>
+  void adt_score_kernel<queue_cpp>::operator()();
+} // namespace mudock
---
+++ mudock/include/mudock/cpp_implementation/genetic_cpp.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <mudock/batch.hpp>
+#include <mudock/compute/genetic.hpp>
+#include <mudock/compute/scratchpad.hpp>
+#include <mudock/cpp_implementation/queue_cpp.hpp>
+#include <random>
+
+namespace mudock {
+  template<>
+  struct rand_state_type<queue_cpp> {
+    using type = std::mt19937;
+  };
+
+  template<>
+  void genetic_kernel<queue_cpp>::operator()();
+  template<>
+  void genetic_kernel<queue_cpp>::initialize();
+  template<>
+  void genetic_kernel<queue_cpp>::finalize();
+} // namespace mudock
