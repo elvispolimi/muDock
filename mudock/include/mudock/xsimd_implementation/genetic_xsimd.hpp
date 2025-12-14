3a777d29522a6006ad19ee679e99bda94f1f182d
gaccordi <10587213@polimi.it>
2025-12-13 16:20:16 -0800

    Adapt XSIMD version to pipeline
    Still the XSIMD version does not work

---
+++ mudock/include/mudock/xsimd_implementation/genetic_xsimd.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <mudock/batch.hpp>
+#include <mudock/compute/genetic.hpp>
+#include <mudock/compute/scratchpad.hpp>
+#include <mudock/cpp_implementation/genetic_cpp.hpp>
+#include <mudock/xsimd_implementation/queue_xsimd.hpp>
+#include <random>
+
+namespace mudock {
+  template<>
+  struct rand_state_type<queue_xsimd> {
+    using type = std::mt19937;
+  };
+
+  template<>
+  struct genetic_kernel<queue_xsimd>: genetic_kernel<queue_cpp> {
+    genetic_kernel(const int batch_ligands_,
+                   const int population_number_,
+                   const int num_generations_,
+                   const int tournament_length_,
+                   const int mutation_prob_,
+                   const size_t seed_,
+                   chromosome* population_,
+                   chromosome* next_population_,
+                   int* __restrict__ num_rotamers_b_,
+                   fp_type* __restrict__ scores_b_,
+                   fp_type* __restrict__ best_scores_b_,
+                   chromosome* __restrict__ best_chromosomes_b_)
+        : genetic_kernel<queue_cpp>(batch_ligands_,
+                                    population_number_,
+                                    num_generations_,
+                                    tournament_length_,
+                                    mutation_prob_,
+                                    seed_,
+                                    population_,
+                                    next_population_,
+                                    num_rotamers_b_,
+                                    scores_b_,
+                                    best_scores_b_,
+                                    best_chromosomes_b_) {};
+    genetic_kernel() {};
+  };
+} // namespace mudock
