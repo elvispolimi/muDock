9247532769ec3072ee0279c9e5ec190d63f6648d
gaccordi <10587213@polimi.it>
2025-12-19 09:45:33 -0800

    Add Support for CUDA

--- mudock/include/mudock/cpp_implementation/adt_score_cpp.hpp
+++ mudock/include/mudock/cpp_implementation/adt_score_cpp.hpp
@@ -4,6 +4,10 @@
 #include <mudock/cpp_implementation/queue_cpp.hpp>
 
 namespace mudock {
+  template<>
+  inline int get_adt_score_batch<queue_cpp>(const int) {
+    return 10;
+  }
 
   template<>
   void adt_score_kernel<queue_cpp>::operator()();
