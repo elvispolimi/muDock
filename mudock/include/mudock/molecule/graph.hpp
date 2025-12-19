dad298d59acf6947bdf616f64e6b9cb0da6abedb
gaccordi <10587213@polimi.it>
2025-12-19 09:43:38 -0800

    Fix boost warning about undefined memory allocation

--- mudock/include/mudock/molecule/graph.hpp
+++ mudock/include/mudock/molecule/graph.hpp
@@ -8,10 +8,10 @@ namespace mudock {
 
   // graph description
   struct molecule_edge_type {
-    int bond_index;
+    int bond_index{0};
   };
   struct molecule_vertex_type {
-    int atom_index;
+    int atom_index{0};
   };
   using molecule_graph_type = boost::
       adjacency_list<boost::setS, boost::vecS, boost::undirectedS, molecule_vertex_type, molecule_edge_type>;
