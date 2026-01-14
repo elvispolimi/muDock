d2c453bd4508672517bcba8a2ed189c5cb84ce45
gaccordi <10587213@polimi.it>
2026-01-13 23:08:25 -0800

    First SYCL version

--- mudock/include/mudock/sycl_implementation/sycl_implementation.hpp
+++ mudock/include/mudock/sycl_implementation/sycl_implementation.hpp
@@ -2,7 +2,7 @@
 
 #if defined(MUDOCK_USE_SYCL)
   #include <mudock/sycl_implementation/adt_score_sycl.hpp>
-  // #include <mudock/sycl_implementation/genetic_sycl.hpp>
-  // #include <mudock/sycl_implementation/geom_transform_sycl.hpp>
+  #include <mudock/sycl_implementation/genetic_sycl.hpp>
+  #include <mudock/sycl_implementation/geom_transform_sycl.hpp>
   #include <mudock/sycl_implementation/queue_sycl.hpp>
 #endif
--- mudock/src/sycl_implementation/sycl_random.cpp
+++ mudock/src/sycl_implementation/sycl_random.cpp
@@ -1,16 +1,18 @@
+#include <chrono>
 #include <mudock/sycl_implementation/sycl_random.hpp>
 #include <random>
 
 namespace mudock {
 
   void sycl_random_object::alloc(const std::size_t num_elements, const std::size_t seed) {
-    const bool init = num_elements > sycl_wrapper<std::vector, XORWOWState>::num_elements();
-    sycl_wrapper<std::vector, XORWOWState>::alloc(num_elements);
-    auto generator = std::mt19937{static_cast<unsigned>(seed)};
-    auto dist      = std::uniform_int_distribution();
+    const bool init = num_elements > state.num_elements();
+    state.alloc(num_elements);
     if (init) {
-      for (auto& s: host) { s.set_seed(dist(generator)); }
-      sycl_wrapper<std::vector, XORWOWState>::copy_host2device();
+      auto generator = std::mt19937{static_cast<unsigned>(seed)};
+      auto dist      = std::uniform_int_distribution();
+      for (size_t i = 0; i < num_elements; i++) state()[i].set_seed(dist(generator));
+      state.copy_host2device();
+      q->synchronize();
     }
   };
   void sycl_random_object::alloc(const std::size_t num_elements) {
