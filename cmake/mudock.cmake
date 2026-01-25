5c7e9d4ccbfdbb98599522cdb4b0e16962e6729d
gianfi12 <10587213@polimi.it>
2026-01-25 17:10:26 +0900

    minor fixes

--- cmake/mudock.cmake
+++ cmake/mudock.cmake
@@ -4,9 +4,10 @@
 
 function(hip_separable_compilation target)
   if(CMAKE_HIP_PLATFORM STREQUAL "nvidia")
+    message(STATUS "----here ${target}")
     set(MUDOCK_LIB "$<TARGET_FILE:libmudock>")
     set(OBJECTS $<TARGET_OBJECTS:${target}>)
-    set(DEVICE_LINK_OBJ ${CMAKE_CURRENT_BINARY_DIR}/link/${target}.o)
+    set(DEVICE_LINK_OBJ ${CMAKE_CURRENT_BINARY_DIR}/${target}.o)
 
     separate_arguments(flags UNIX_COMMAND PROGRAM ${CMAKE_HIP_FLAGS})
 
--- mudock/src/hip_implementation/adt_score_hip.cpp
+++ mudock/src/hip_implementation/adt_score_hip.cpp
@@ -326,14 +326,11 @@ namespace mudock {
     constexpr_for<0, reorder_buffer<static_molecule>::get_num_atom_clusters(), 1>([&](const auto atom_index) {
       const auto n_atoms = reorder_buffer<static_molecule>::atoms_clusters[atom_index];
       if (atoms == n_atoms)
-        bucket_size = get_evaluate_fitness_batch<n_atoms>(device_id);
+        bucket_multiple = get_evaluate_fitness_batch<n_atoms>(device_id);
     });
     if (bucket_multiple == 0)
       throw std::runtime_error(
           "Compilation error: there is a bucket of atoms number which it is not handled.");
-
-    mudock::info("Hip Bucket size for ", atoms, " atoms ", bucket_multiple * BUCKET_MULTIPLIER, " ligands.");
-    return bucket_multiple * BUCKET_MULTIPLIER;
 
     int bucket_size = max_bucket_size / bucket_multiple;
     bucket_size     = bucket_size == 0 ? max_bucket_size : bucket_size * bucket_multiple;
