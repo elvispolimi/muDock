ea44c60614bf238d4d2f228251c8c36d0e53664d
gaccordi <10587213@polimi.it>
2026-02-09 00:17:40 -0800

    Move the score generatiosn from variable to macro

--- mudock/include/mudock/compute/adt_score.hpp
+++ mudock/include/mudock/compute/adt_score.hpp
@@ -243,11 +243,11 @@ namespace mudock {
           (((*this->scratch).template get<buffer_data_type::SCORES>().num_elements() % batch_ligands) == 0) &&
           "Number of scores is not a multiple of ligands in the batch");
       assert(kernel && "Kernel method not yet prepared");
-      if constexpr (knobs::num_score_generations > 1) {
-        for (size_t i = 0; i < knobs::num_score_generations; ++i) (*kernel)();
-      } else {
-        (*kernel)();
-      }
+  #ifdef MUDOCK_SCORE_GENERATIONS
+      for (size_t i = 0; i < MUDOCK_SCORE_GENERATIONS; ++i) (*kernel)();
+  #else
+      (*kernel)();
+  #endif
     }
 
     static int get_ligand_mem(const int max_atoms, const knobs conf) {
