25963922a1fe4f79fec45f07e7a00127cda3ef29
gaccordi <10587213@polimi.it>
2026-02-08 23:34:27 -0800

    Add conf for the benchmarking of the scoring steps

--- mudock/include/mudock/knobs.hpp
+++ mudock/include/mudock/knobs.hpp
@@ -7,11 +7,12 @@
 namespace mudock {
 
   struct knobs {
-    std::size_t population_number   = 100;
-    std::size_t num_generations     = 1000;
-    std::size_t tournament_length   = 10;
-    fp_type mutation_prob           = fp_type{0.01};
-    std::optional<std::size_t> seed = std::optional<std::size_t>{};
+    std::size_t population_number                      = 100;
+    std::size_t num_generations                        = 1000;
+    static constexpr std::size_t num_score_generations = 1;
+    std::size_t tournament_length                      = 10;
+    fp_type mutation_prob                              = fp_type{0.01};
+    std::optional<std::size_t> seed                    = std::optional<std::size_t>{};
   };
 
 } // namespace mudock
