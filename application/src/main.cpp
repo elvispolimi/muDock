f8c2d91dfb82880ceabdfa250391d6c90c4696c3
gaccordi <10587213@polimi.it>
2026-02-17 10:10:26 -0800

    Fix warning

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -41,7 +41,9 @@ int main(int argc, char* argv[]) {
         // parse the input ligands and put them in a stack that we can compute
         mudock::info("Parsing ", ligands_description.size(), " ligand(s) ...");
         if constexpr (format == mudock::supported_format::ADTMOL2) {
+#ifdef _OPENMP
 #pragma omp parallel for shared(input_queue)
+#endif
           for (const auto& description: ligands_description) {
             auto ligand = std::make_unique<mudock::static_molecule>(
                 mudock::parser<mudock::supported_format::ADTMOL2, mudock::static_molecule>(description));
