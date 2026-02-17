e0c089bc184a5c3fab75308363b28bc0bc147229
gaccordi <10587213@polimi.it>
2026-02-16 22:35:53 -0800

    Fix HIP clash in num device variable

--- mudock/src/hip_implementation/queue_hip.cpp
+++ mudock/src/hip_implementation/queue_hip.cpp
@@ -11,7 +11,7 @@
 namespace mudock {
 #ifdef MUDOCK_KERNEL_LOCK
   namespace {
-    constexpr int k_max_devices = 16;
+    constexpr int k_max_devices_kernel_lock = 16;
 
     struct device_kernel_lock {
       std::mutex mutex;
@@ -26,7 +26,7 @@ namespace mudock {
       }
     };
 
-    device_memory_array<k_max_devices, device_kernel_lock> hip_kernel_locks;
+    device_memory_array<k_max_devices_kernel_lock, device_kernel_lock> hip_kernel_locks;
 
     device_kernel_lock* get_kernel_lock(const int dev) {
       hip_kernel_locks.init(dev, std::function<std::unique_ptr<device_kernel_lock>()>([&]() {
