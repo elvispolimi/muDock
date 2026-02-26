d2318cefca0b0335a34c6d70cf8a57df3f4e37e5
gaccordi <gianmarco.accordi@polimi.it>
2026-02-26 18:07:12 +0000

    Fix how bucket multiple ovverride is used

--- mudock/include/mudock/compute/bucket_size.hpp
+++ mudock/include/mudock/compute/bucket_size.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
 #include <mudock/log.hpp>
 #include <stdexcept>
@@ -22,17 +23,34 @@ namespace mudock {
                  capped);
     return capped;
 #else
-  #ifdef MUDOCK_ADT_BUCKET_MULTIPLE_OVERRIDE
-    const int bucket_multiple = MUDOCK_ADT_BUCKET_MULTIPLE_OVERRIDE;
-  #else
     const int bucket_multiple = get_default_multiple();
-    if (bucket_multiple == 0)
+    if (bucket_multiple <= 0)
       throw std::runtime_error(
           "Compilation error: there is a bucket of atoms number which it is not handled.");
-  #endif
 
     int bucket_size = max_bucket_size / bucket_multiple;
     bucket_size     = bucket_size == 0 ? max_bucket_size : bucket_size * bucket_multiple;
+
+  #ifdef MUDOCK_ADT_BUCKET_MULTIPLE_OVERRIDE
+    const int base_bucket_size = bucket_size;
+    const double override_multiplier = static_cast<double>(MUDOCK_ADT_BUCKET_MULTIPLE_OVERRIDE);
+    if (override_multiplier <= 0.0) {
+      throw std::runtime_error(
+          "Compilation error: MUDOCK_ADT_BUCKET_MULTIPLE_OVERRIDE must be > 0.");
+    }
+    const int scaled_bucket_size =
+        static_cast<int>(std::llround(static_cast<double>(bucket_size) * override_multiplier));
+    bucket_size = std::max<int>(1, scaled_bucket_size);
+    mudock::info(backend_name,
+                 " Bucket size for ",
+                 atoms,
+                 " atoms base bucket size ",
+                 base_bucket_size,
+                 " scaled by override ",
+                 override_multiplier,
+                 " -> ",
+                 bucket_size);
+  #else
     mudock::info(backend_name,
                  " Bucket size for ",
                  atoms,
@@ -42,6 +60,7 @@ namespace mudock {
                  max_bucket_size,
                  " max bucket size -> ",
                  bucket_size);
+  #endif
     return bucket_size;
 #endif
   }
