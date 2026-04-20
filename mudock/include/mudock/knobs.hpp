86e93eb332e3164f35a6ade563b991075df1a0d6
gianfi12 <10587213@polimi.it>
2026-04-20 00:26:34 -0700

    Generalize and harden the TBB pipeline

--- mudock/include/mudock/knobs.hpp
+++ mudock/include/mudock/knobs.hpp
@@ -14,6 +14,7 @@ namespace mudock {
     std::optional<std::size_t> seed                    = std::optional<std::size_t>{};
     std::size_t max_tbb_tokens                         = 4;
     std::size_t max_bytes_per_token                    = 1048576; // 1 MB
+    std::size_t max_tbb_queue_size                     = 100000;
   };
 
 } // namespace mudock
--- mudock/include/mudock/tbb_implementation/parser_filter.hpp
+++ mudock/include/mudock/tbb_implementation/parser_filter.hpp
@@ -1,18 +1,31 @@
 #pragma once
 
+#include <atomic>
 #include <memory>
+#include <mudock/compute/safe_queue.hpp>
+#include <mudock/format/supported_format.hpp>
 #include <mudock/molecule.hpp>
 #include <mudock/mudock.hpp>
 #include <string_view>
-#include <vector>
 
 namespace mudock {
-    
-    class parser_filter {
-        public:
-            using mol_vec = std::vector<std::unique_ptr<static_molecule>>;
 
-            mol_vec operator()(std::string_view sv) const;
-    };
+  template<supported_format format>
+  class parser_filter {
+  private:
+    std::shared_ptr<safe_queue<static_molecule>> input_queue;
+    std::atomic<std::size_t>* skipped_ligands = nullptr;
+    std::atomic<bool>* stop_requested         = nullptr;
+
+  public:
+    explicit parser_filter(std::shared_ptr<safe_queue<static_molecule>> input,
+                           std::atomic<std::size_t>* skipped = nullptr,
+                           std::atomic<bool>* stop = nullptr)
+        : input_queue(std::move(input)),
+          skipped_ligands(skipped),
+          stop_requested(stop) {}
+
+    void operator()(std::string_view sv) const;
+  };
 
 } // namespace mudock
