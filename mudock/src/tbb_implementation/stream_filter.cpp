f4a2b4b1a7fd507d7dbbba8b9c7df418c92642b4
gianfi12 <10587213@polimi.it>
2026-04-21 09:49:48 -0700

    Reduce TBB stream buffering and make enqueue explicit

--- mudock/src/tbb_implementation/stream_filter.cpp
+++ mudock/src/tbb_implementation/stream_filter.cpp
@@ -28,67 +28,65 @@ namespace mudock {
       return stop_requested != nullptr && stop_requested->load(std::memory_order_relaxed);
     };
 
-    auto flush_remainder = [&]() {
-      if (flushed_) {
-        return;
-      }
-      flushed_ = true;
-      if (!buffered_text_.empty()) {
-        ready_tokens_.emplace_back(std::move(buffered_text_));
-        buffered_text_.clear();
-      }
-    };
-
-    while (ready_tokens_.empty()) {
+    while (true) {
       if (should_stop()) {
-        ready_tokens_.clear();
+        buffered_text_.clear();
+        flushed_ = true;
         fc.stop();
         return {};
       }
 
+      if (!buffered_text_.empty()) {
+        auto input_view = std::string_view{buffered_text_};
+        const auto next = format_splitter_.next_molecule_start_index(input_view);
+        if (next != std::string_view::npos) {
+          std::string token = buffered_text_.substr(0, next);
+          buffered_text_.erase(0, next);
+          return token;
+        }
+      }
+
       const std::streampos pos = stream_.tellg();
       if (pos == std::streampos(-1)) {
-        flush_remainder();
-        break;
+        if (!flushed_ && !buffered_text_.empty()) {
+          flushed_ = true;
+          auto token = std::move(buffered_text_);
+          buffered_text_.clear();
+          return token;
+        }
+        fc.stop();
+        return {};
       }
 
       const std::size_t cur = static_cast<std::size_t>(pos);
       if (cur >= end_) {
-        flush_remainder();
-        break;
+        if (!flushed_ && !buffered_text_.empty()) {
+          flushed_ = true;
+          auto token = std::move(buffered_text_);
+          buffered_text_.clear();
+          return token;
+        }
+        fc.stop();
+        return {};
       }
 
       const std::size_t bytes_per_token = std::min(max_bytes_per_token_, end_ - cur);
       std::string buf(bytes_per_token, '\0');
       stream_.read(buf.data(), static_cast<std::streamsize>(buf.size()));
       if (stream_.gcount() <= 0) {
-        flush_remainder();
-        break;
+        if (!flushed_ && !buffered_text_.empty()) {
+          flushed_ = true;
+          auto token = std::move(buffered_text_);
+          buffered_text_.clear();
+          return token;
+        }
+        fc.stop();
+        return {};
       }
 
       buf.resize(static_cast<std::size_t>(stream_.gcount()));
       buffered_text_ += buf;
-      auto input_view = std::string_view{buffered_text_};
-      while (!input_view.empty()) {
-        const auto next = format_splitter_.next_molecule_start_index(input_view);
-        if (next == std::string_view::npos) {
-          buffered_text_ = std::string{input_view};
-          break;
-        }
-
-        ready_tokens_.emplace_back(input_view.substr(0, next));
-        input_view = input_view.substr(next);
-      }
     }
-
-    if (ready_tokens_.empty()) {
-      fc.stop();
-      return {};
-    }
-
-    auto token = std::move(ready_tokens_.front());
-    ready_tokens_.pop_front();
-    return token;
   }
 
   template class stream_filter<supported_format::ADTMOL2>;
