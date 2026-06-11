0493518f0903915b7d2fbd5e1e3983597267eb5b
gianfi12 <10587213@polimi.it>
2026-06-11 19:56:51 +0200

    Separated format ids from parsing output extensions

--- mudock/include/mudock/format/supported_format.hpp
+++ mudock/include/mudock/format/supported_format.hpp
@@ -1,17 +1,12 @@
 #pragma once
 
-#include <array>
-#include <assert.h>
-#include <filesystem>
+#include <mudock/format/format_id.hpp>
 #include <mudock/format/adt_mol2.hpp>
 #include <mudock/format/mol2.hpp>
 #include <mudock/format/pdb.hpp>
 #include <mudock/format/pdbqt.hpp>
-#include <string>
 
 namespace mudock {
-  enum class supported_format : int { MOL2 = 0, PDBQT, PDB, ADTMOL2 };
-
   // Trait: Kind -> Type
   template<supported_format>
   struct type_of_format_t; // primary (no def)
@@ -35,26 +30,4 @@ namespace mudock {
 
   template<supported_format T>
   using type_of_format = typename type_of_format_t<T>::type;
-
-  struct format_description {
-    supported_format format;
-    std::string_view extension;
-  };
-
-  static constexpr std::array<format_description, 4> FORMAT_EXTENSIONS = {
-      {{supported_format::MOL2, "mol2"},
-       {supported_format::PDBQT, "pdbqt"},
-       {supported_format::PDB, "pdb"},
-       {supported_format::ADTMOL2, "adtmol2"}}};
-
-  static constexpr auto get_num_supported_format() { return FORMAT_EXTENSIONS.size(); }
-
-  [[nodiscard]] supported_format parse_supported_format(const std::string_view extension);
-  [[nodiscard]] inline supported_format parse_supported_format(const std::filesystem::path path) {
-    assert(path.has_extension());
-    const auto extension = path.extension();
-    assert(!extension.empty());
-    return parse_supported_format(std::string_view(extension.string().substr(1)));
-  }
-  [[nodiscard]] std::string_view parse_supported_format(const supported_format format);
 } // namespace mudock
