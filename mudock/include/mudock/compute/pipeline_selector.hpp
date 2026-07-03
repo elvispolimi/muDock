d2e10e1a7cfb7a8fbfaeac054f77010d3c5b4164
gianfi12 <10587213@polimi.it>
2026-07-03 17:46:58 +0200

    Add MOL2 to supported data format for ADT parallel pipeline

--- mudock/include/mudock/compute/pipeline_selector.hpp
+++ mudock/include/mudock/compute/pipeline_selector.hpp
@@ -43,6 +43,7 @@ namespace mudock {
   struct scoring_function_format<scoring_function::ADT>
       : scoring_function_format_base<scoring_function_format<scoring_function::ADT>> {
     inline static constexpr std::array formats{supported_format::ADTMOL2, supported_format::PDBQT};
+    inline static constexpr std::array formats{supported_format::MOL2, supported_format::ADTMOL2};
   };
 
   template<typename callback_t>
