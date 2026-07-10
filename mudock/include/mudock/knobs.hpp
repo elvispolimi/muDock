99a48957839d25d32381e97523f79a30e634fe8b
Daniele Piano <dpiano01@gmail.com>
2026-07-10 20:38:20 +0200

    tbb::flow::graph - End-to-end TBB graph with mocked scorer

--- mudock/include/mudock/knobs.hpp
+++ mudock/include/mudock/knobs.hpp
@@ -12,9 +12,12 @@ namespace mudock {
     std::size_t tournament_length   = 10;
     fp_type mutation_prob           = static_cast<fp_type>(0.01);
     std::optional<std::size_t> seed = std::optional<std::size_t>{};
-    std::size_t max_tbb_tokens      = 4;
+    std::size_t max_tbb_tokens      = 4;       // TODO - with flow::graph, it can be removed
     std::size_t max_bytes_per_token = 1048576; // 1 MB
     std::size_t max_tbb_queue_size  = 100000;
+
+    // todo - strategy definition (user-defined vs. automatic; starvation warning)
+    std::size_t max_ligands_in_flight = 100000;
   };
 
 } // namespace mudock
