c3036e1ac91b0db17ad0046986a668a0d21e6e75
lorenzoooov <lorenzovalentini02@gmail.com>
2026-08-21 12:45:02 +0200

    set default lsrate to 100%, the user should decide the custom rate if needed

--- mudock/include/mudock/knobs.hpp
+++ mudock/include/mudock/knobs.hpp
@@ -14,7 +14,7 @@ namespace mudock {
     bool autostop                             = false;
     std::size_t convergence_window            = 5;
     fp_type variance_threshold                = static_cast<fp_type>(1e-4);
-    fp_type lsrate                            = static_cast<fp_type>(50);
+    fp_type lsrate                            = static_cast<fp_type>(100);
     bool ls_on_best                           = false;
     std::size_t lsit                          = 300;
     std::size_t ls_every                      = 1;
