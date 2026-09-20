31a35f0a0d090bd677efc62455cdebdec886ab24
lorenzoooov <lorenzovalentini02@gmail.com>
2026-09-20 15:54:55 +0200

    add casf crystals experiment

--- lorenzo_temp/experiments/crystal/run_crystals.sh
+++ lorenzo_temp/experiments/crystal/run_crystals.sh
@@ -1,47 +1,31 @@
 #!/usr/bin/env bash
 
-MAX_JOBS=6
-
 BUILD=omp
+DATA_DIR="./data/crystals"
 
-ids=("1fkb" "1hii" "2ya6" "3udd" "4few" "5cst" "5uez" "5wuk")
-lsits=("10" "25" "50" "100" "200")
-
-TOTAL_RUNS=$((${#ids[@]} * ${#lsits[@]}))
+dirs=($DATA_DIR/*/)
+TOTAL_RUNS=${#dirs[@]}
 COMPLETED_RUNS=0
 
+echo "Running Local Search on crystals..."
 
-echo "Docking..."
-for PDBID in "${ids[@]}"; do
-    for LSIT in "${lsits[@]}"; do
-        PROTEIN="./data/${PDBID}/${PDBID}_protein.pdb"
-        LIGAND="./data/${PDBID}/${PDBID}_ligand.adtmol2"
+for dir in "${dirs[@]}"; do
+    PDBID=$(basename "$dir")
 
-        ./builds/"$BUILD"/application/local_search/local_search \
-            --protein "$PROTEIN" \
-            --ligand "$LIGAND" \
-            --lsit "$LSIT" \
-            > /dev/null 2>&1 \
-            &
-        
-        if [ "$(jobs -rp | wc -l)" -ge "$MAX_JOBS" ]; then
-            wait -n
-            COMPLETED_RUNS=$((COMPLETED_RUNS + 1))
-            printf "\rProgress: [%d/%d] %3d%%" \
-                "$COMPLETED_RUNS" \
-                "$TOTAL_RUNS" \
-                "$((COMPLETED_RUNS * 100 / TOTAL_RUNS))"
-        fi
-    done      
-done
+    PROTEIN="${DATA_DIR}/${PDBID}/${PDBID}_protein.pdb"
+    LIGAND="${DATA_DIR}/${PDBID}/${PDBID}_ligand.adtmol2"
 
-while [ "$(jobs -rp | wc -l)" -gt 0 ]; do
-    wait -n
+    ./builds/"$BUILD"/application/local_search/local_search \
+        --protein "$PROTEIN" \
+        --ligand "$LIGAND" \
+        > /dev/null 2>&1
+    
     COMPLETED_RUNS=$((COMPLETED_RUNS + 1))
     printf "\rProgress: [%d/%d] %3d%%" \
         "$COMPLETED_RUNS" \
         "$TOTAL_RUNS" \
         "$((COMPLETED_RUNS * 100 / TOTAL_RUNS))"
+     
 done
 
 python3 lorenzo_temp/experiments/notify.py
