893ed3e3c7638cb30c43565dcc6a64e8d67c5fae
lorenzoooov <lorenzovalentini02@gmail.com>
2026-09-22 14:43:25 +0200

    fix output

--- mudock/include/mudock/tbb_implementation/tbb_pipeline.hpp
+++ mudock/include/mudock/tbb_implementation/tbb_pipeline.hpp
@@ -47,7 +47,6 @@ namespace mudock {
       buf += ligand.properties.get(property_type::RMSD_BEST_SCORING_POSE);
       buf += ',';
       buf += ligand.properties.get(property_type::RMSD_MIN);
-      buf += ',';
       buf += '\n';
     }
   } // namespace detail
